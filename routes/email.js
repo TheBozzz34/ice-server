b3fa7524beac0454e5d0370768ba02ed8964c21d
Ethan James <necrozma@catgirlsaresexy.org>
2024-05-01 21:05:07 -0700

    Initial Commit

---
+++ routes/email.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var fs = require('fs');
+var supabase = require('../supabase');
+
+require.extensions['.html'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+var emailTemplate = require('..//email.html');
+
+const adminAuthClient = supabase.auth.admin
+
+async function checkUser(userId) {
+    const { data, error } = await supabase.auth.admin.getUserById(userId)
+    if (error) {
+        console.error(error)
+        return false
+    } else {
+        return true
+    }
+}
+
+/* send new email. */
+router.post('/', async function(req, res, next) { // Note the async keyword here
+    const sendTo = req.body.email;
+    const employeeName = req.body.name;
+    const userId = req.body.userId;
+
+    if (!sendTo || !employeeName || !userId) {
+        res.status(400).send('Missing required fields');
+        return;
+    }
+
+    const user = await checkUser(userId); // Await the result of checkUser
+
+    if (!user) {
+        res.status(400).send('User not found');
+        return;
+    }
+
+    let custom = emailTemplate.replace('{{employee_name}}', employeeName);
+
+    const options = {
+        url: 'https://api.resend.com/emails',
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.RESEND_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        json: true,
+        body: {
+            "from": "onboarding@resend.dev",
+            "to": sendTo,
+            "subject": "Welcome to Wheeler Peak Ice!",
+            "html": custom
+        }
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log(response.statusCode);
+            console.log(body);
+            res.send(body);
+        }
+    });
+});
+
+
+module.exports = router;
---
+++ routes/export.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var supabase = require('../supabase');
+const { convertArrayToCSV } = require('convert-array-to-csv');
+const fs = require('fs');
+
+async function checkUser(userId) {
+    const { data, error } = await supabase.auth.admin.getUserById(userId)
+    if (error) {
+        console.error(error)
+        return false
+    } else {
+        return true
+    }
+}
+
+router.get('/', async function(req, res, next) { // Note the async keyword here
+    const userId = req.body.userId;
+
+    if (!userId) {
+        res.status(400).send('Missing required fields');
+        return;
+    }
+
+    const user = await checkUser(userId); // Await the result of checkUser
+
+    if (!user) {
+        res.status(400).send('User not found');
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from('rounds')
+        .select()
+
+    if (error) {
+        console.error(error)
+        res.status(500).send('Error fetching data')
+    }
+
+    if (data) {
+        const csvFromArrayOfObjects = convertArrayToCSV(data);
+        fs.writeFileSync('rounds.csv', csvFromArrayOfObjects);
+        res.download('rounds.csv');
+    } else {
+        res.status(500).send('Error fetching data')
+    }
+});
+
+
+module.exports = router;
